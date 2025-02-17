d42ded9a1eb4f179a80b116f50729e5c1a5ebb49
Narek <narekbayanduryan16@gmail.com>
2025-02-18 02:51:52 +0300

    Wrote by consumer and fixed a couple of bugs in JS.

--- balabolka_chat/asgi.py
+++ balabolka_chat/asgi.py
@@ -1,13 +1,22 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from chat.routing import websocket_urlpatterns
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "balabolka_chat.settings")
 
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter(
 	{
-		"http": get_asgi_application(),
+		"http": django_asgi_app,
+		"websocket": AllowedHostsOriginValidator(
+			AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+		),
 	}
 )
