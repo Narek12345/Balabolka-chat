b994b3f20f2d2fb198c6311664f9b90e4410813e
Narek <narekbayanduryan16@gmail.com>
2025-02-18 03:09:29 +0300

    Added a channel layer to settings.

--- balabolka_chat/settings.py
+++ balabolka_chat/settings.py
@@ -131,3 +131,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 ASGI_APPLICATION = "balabolka_chat.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        }
+    }
+}
